55f4db35b57eb58146fc1c55ab74ecf3bf37a96b
kevina <kevinawongw@gmail.com>
2023-05-23 20:04:55 -0700

    style: lint

--- utils.py
+++ utils.py
@@ -1,22 +1,33 @@
-import re 
+"""
+    Helper funcs and other utils.
+"""
+import re
+
 def check_string(string):
+    """
+        Checks for correct formatting.
+    """
     pattern = r'^[a-zA-Z_*]{5}$'
     match = re.match(pattern, string)
+
     return bool(match)
 
-       
 def read_words(filename):
-    f = open(filename, "r")
-    data = f.read()
+    """
+        Reads list of 5 letter words.
+    """
+    file = open(filename, "r", encoding="utf-8")
+    data = file.read()
     full_list = data.split('\n')
-    f.close()
+    file.close()
     return full_list
 
 def insist_correct(result):
+    """
+        Asks until correct formatting.
+    """
     while not check_string(result):
         print("Invalid format. Please try agian.")
-        result = input("Input Result:") 
+        result = input("Input Result:")
     return result
-
-
     
